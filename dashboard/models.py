950b94f83d6c81e0cf3d695e620787f5698832c2
Ramiro Chacón <ramiro20010202@gmail.com>
2024-12-21 14:12:26 -0600

    add model professor

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True, null=False)
+    age = models.IntegerField()
